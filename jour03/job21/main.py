7f69b883b89eb2fe564bf7369b23e2ef3dc169b8
enzo-mandine <55872776+enzo-mandine@users.noreply.github.com>
2021-10-01 23:29:45 +0200

    update

--- jour03/job21/main.py
+++ jour03/job21/main.py
@@ -1,20 +1,47 @@
 import re
+import string
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sort(data):
+    sortedData = {}
+    for i in data:
+        sortedData.update({i: {}})
+        # sorted(data[i].items(), key=lambda kv: [kv[1], kv[0]], reverse=True)
+        # print(i, sorted(data[i].items(), key=lambda kv: [kv[1], kv[0]], reverse=True))
+        for j in sorted(data[i].items(), key=lambda kv: [kv[1], kv[0]], reverse=True):
+            # print(i, j[0], j[1])
+            sortedData[i].update({j[0]: j[1]})
+    return sortedData
+
 
 file = open('data.txt', 'r')
 read = file.read()
 file.close()
 data = re.findall(r"[a-zA-Z][a-zA-Z]", read, re.I)
 result = {}
-i = ord('a')
-while i <= ord('z'):
-    j = ord('a')
-    result.update({chr(i): {}})
-    while j <= ord('z'):
-        result[chr(i)].update({chr(j): 0})
-        j += 1
-    i += 1
+abc = list(string.ascii_lowercase)
+
+for i in abc:
+    result.update({i: {'total': 0}})
+    for j in abc:
+        result[i].update({j: 0})
 
 for i in data:
     result[i[0].lower()][i[1].lower()] += 1
+    result[i[0].lower()]['total'] += 1
 
-print(result['a'])
+for i in abc:
+    total = result[i]['total']
+    result[i].pop('total')
+    for j in abc:
+        if result[i].get(j) != 0:
+            result[i].update({j: result[i][j] * 100 / total})
+
+result = sort(result)
+for i in abc:
+    plt.title(i)
+    for j in result[i]:
+        plt.bar(j, result[i][j])
+    plt.show()
