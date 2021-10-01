dee10f7568c248ab027054e32cf42124490f9ca2
enzo-mandine <55872776+enzo-mandine@users.noreply.github.com>
2021-10-01 17:37:37 +0200

    rename function to clarify usage

--- jour04/job08/main.py
+++ jour04/job08/main.py
@@ -8,7 +8,7 @@ def defineX(d, posX):
     return posX
 
 
-def placeCheckers(board, posY, posX):
+def placeQueen(board, posY, posX):
     d = len(board)
     board[posY][posX] = "X"
     posX = defineX(d, posX)
@@ -17,7 +17,7 @@ def placeCheckers(board, posY, posX):
     if posX == d or posY == d:
         return printBoard(board)
 
-    return placeCheckers(board, posY, posX)
+    return placeQueen(board, posY, posX)
 
 
 def printBoard(board):
@@ -31,8 +31,8 @@ n = int(input('i: '))
 board = [['O' for x in range(n)] for y in range(n)]
 
 if n > 3 and n % 3 == 0:
-    placeCheckers(board, 0, 1)
+    placeQueen(board, 0, 1)
 elif n > 3 and n % 2 != 0:
-    placeCheckers(board, 0, 0)
+    placeQueen(board, 0, 0)
 else:
     print('operation impossible')
